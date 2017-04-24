d5a7d0ced4f94950816448b988bb031ce77799be
Rubén Torrero Marijnissen <r.torrero@kentech-sp.com>
2017-04-24 16:37:21 +0100

    Initial commit

---
+++ title-blackifier.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "$0: usage: title-blackifier.sh <search string> <command>"
+	exit 1
+fi
+
+search_string=$1
+command=$2
+
+echo "about to run command"
+nohup spotify >/dev/null&
+echo "Polling wmctrl for window id..."
+
+# Find all webstorm window IDs and save them to a variable
+instances=$(wmctrl -l | grep $search_string | cut -d" " -f1 | wc -l)
+while [ $instances -le 0 ]
+do
+	sleep 0.1
+	instances=$(wmctrl -l | grep $search_string | cut -d" " -f1 | wc -l)
+done
+
+windowIds=$(wmctrl -l | grep $search_string | cut -d" " -f1)
+echo "Target found, proceed..."
+
+echo $windowIds
+
+# Loop and set all windows to GTK dark variant
+while read -r line; do
+	xprop -f _GTK_THEME_VARIANT 8u -set _GTK_THEME_VARIANT "dark" -id $line
+done <<< $windowIds
